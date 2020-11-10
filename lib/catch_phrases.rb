ad113a1b835276ab9955796aa8739a3c65f2b34b
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-10 03:33:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,13 +7,11 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+  puts $status
 end
 
 def link
-  saying = "It's Dangerous To Go Alone! Take This."
-  puts saying
+  puts $saying
 end
 
 def all_phrases(mario, toadstool, link)
